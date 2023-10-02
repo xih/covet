49ec5ff3b27fb43f6e81ba52f07705d7082d76e8
Dennis <dennis@momentum.local>
2023-10-01 17:09:19 -0700

    add MVP sign in flow with google auth

---
+++ src/components/ui/UserAuth.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export default function UserAuth() {
+  const { data: sessionData } = useSession();
+
+  return (
+    <button
+      className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+      onClick={sessionData ? () => void signOut() : () => void signIn()}
+    >
+      {sessionData ? "Sign out" : "Sign in"}
+    </button>
+  );
+}
