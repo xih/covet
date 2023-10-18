6fa79bbeeaad6a3fa505a60ea93fdd6d9d9a9039
Dennis <dennis@momentum.local>
2023-10-18 14:57:17 -0700

    add schema file and new env

--- src/env.mjs
+++ src/env.mjs
@@ -7,13 +7,13 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    // DATABASE_URL: z
-    //   .string()
-    //   .url()
-    //   .refine(
-    //     (str) => !str.includes("YOUR_MYSQL_URL_HERE"),
-    //     "You forgot to change the default URL",
-    //   ),
+    DATABASE_URL: z
+      .string()
+      .url()
+      .refine(
+        (str) => !str.includes("YOUR_MYSQL_URL_HERE"),
+        "You forgot to change the default URL",
+      ),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
@@ -51,7 +51,7 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
-    // DATABASE_URL: process.env.DATABASE_URL,
+    DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     NEXT_PUBLIC_MAPBOX_TOKEN: process.env.NEXT_PUBLIC_MAPBOX_TOKEN,
     NEXT_PUBLIC_MIXPANEL_TOKEN: process.env.NEXT_PUBLIC_MIXPANEL_TOKEN,
