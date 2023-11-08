d2b846d254501bf4dc44d848d3e88d87f1bc6a4b
neallseth <32910906+neallseth@users.noreply.github.com>
2023-11-07 16:31:03 -0800

    feat(search): adding suggestion footer

--- src/components/DeckMap.tsx
+++ src/components/DeckMap.tsx
@@ -12,7 +12,7 @@ import { useRouter } from "next/router";
 import { useUser } from "@clerk/nextjs";
 import { useDebounce } from "~/lib/hooks";
 import { Button } from "./ui/button";
-import { Moon, Map as LucideMap } from "lucide-react";
+import { Moon, Map as LucideMap, Plus } from "lucide-react";
 import { Badge } from "~/components/ui/badge";
 
 import {
@@ -57,7 +57,15 @@ const INITIAL_VIEW_STATE: ViewState = {
   transitionDuration: 1000,
 };
 
+const INITIAL_SEARCH_SUGGESTION_COUNT = 15;
+
 const cleanedData = data as DataPoint[];
+
+const blue700 = [29, 78, 216];
+const orange500 = [249, 115, 22];
+const rose300 = [253, 164, 175];
+const rose500 = [244, 63, 94];
+const slate300 = [203, 213, 225];
 
 export default function DeckMap() {
   const [searchValue, setSearchValue] = useState("");
@@ -73,6 +81,9 @@ export default function DeckMap() {
   const analyticsSearchValue = useDebounce(searchValue, 1250);
   const darkMapStyle = "mapbox://styles/mapbox/dark-v11";
   const satelliteMapStyle = "mapbox://styles/mapbox/satellite-v9";
+  const [searchSuggestionCount, setSearchSuggestionCount] = useState(
+    INITIAL_SEARCH_SUGGESTION_COUNT,
+  );
 
   const [isSatelliteMapStyle, setIsSatelliteMapStyle] = useState(true);
   const [viewState, setViewState] = useState(INITIAL_VIEW_STATE);
@@ -105,64 +116,59 @@ export default function DeckMap() {
   }, [debouncedSearchValue]);
 
   useEffect(() => {
+    setSearchSuggestionCount(INITIAL_SEARCH_SUGGESTION_COUNT);
+  }, [debouncedSearchValue]);
+
+  useEffect(() => {
     if (analyticsSearchValue) {
       mixpanel.track("search", { query: analyticsSearchValue });
     }
   }, [analyticsSearchValue]);
 
-  // useEffect(() => {
-  //   setSelectedIndex(null);
-  // }, [searchValue]);
+  const ScatterPlayLayer = useMemo(() => {
+    return new ScatterplotLayer<DataPoint>({
+      id: "scatterplot-layer",
+      data: data,
+      pickable: true,
+      opacity: 0.8,
+      filled: true,
+      radiusScale:
+        data.length > 100000
+          ? 5
+          : data.length > 10000
+          ? 10
+          : data.length > 100
+          ? 20
+          : 50,
+      radiusMinPixels: 1,
+      radiusMaxPixels: 100,
+      lineWidthMinPixels: 0,
+      getPosition: (d) => [d.lon, d.lat],
+      getRadius: (d, context) => {
+        if (selectedIndex !== undefined && context.index === selectedIndex) {
+          return 1.2;
+        }
+        return 1;
+      },
+      // @ts-ignore ignore color
+      getFillColor: (d, context) => {
+        if (selectedIndex !== undefined && context.index === selectedIndex) {
+          return isSatelliteMapStyle ? blue700 : orange500; // orange on select
+        }
 
-  const blue700 = [29, 78, 216];
-  const orange500 = [249, 115, 22];
-  const rose300 = [253, 164, 175];
-  const rose500 = [244, 63, 94];
-
-  const slate300 = [203, 213, 225];
-
-  const ScatterPlayLayer = new ScatterplotLayer<DataPoint>({
-    id: "scatterplot-layer",
-    data: data,
-    pickable: true,
-    opacity: 0.8,
-    filled: true,
-    radiusScale:
-      data.length > 100000
-        ? 5
-        : data.length > 10000
-        ? 10
-        : data.length > 100
-        ? 20
-        : 50,
-    radiusMinPixels: 1,
-    radiusMaxPixels: 100,
-    lineWidthMinPixels: 0,
-    getPosition: (d) => [d.lon, d.lat],
-    getRadius: (d, context) => {
-      if (selectedIndex !== undefined && context.index === selectedIndex) {
-        return 1.2;
-      }
-      return 1;
-    },
-    // @ts-ignore ignore color
-    getFillColor: (d, context) => {
-      if (selectedIndex !== undefined && context.index === selectedIndex) {
-        return isSatelliteMapStyle ? blue700 : orange500; // orange on select
-      }
-
-      return isSatelliteMapStyle ? rose300 : slate300; // light black
-    },
-    updateTriggers: {
-      getFillColor: [selectedIndex, isSatelliteMapStyle],
-      getRadius: [selectedIndex],
-    },
-    // highlightedObjectIndex: selectedIndex,
-    autoHighlight: true,
-    highlightColor: () => {
-      return isSatelliteMapStyle ? [244, 63, 94] : [100, 116, 139];
-    },
-  });
+        return isSatelliteMapStyle ? rose300 : slate300; // light black
+      },
+      updateTriggers: {
+        getFillColor: [selectedIndex, isSatelliteMapStyle],
+        getRadius: [selectedIndex],
+      },
+      // highlightedObjectIndex: selectedIndex,
+      autoHighlight: true,
+      highlightColor: () => {
+        return isSatelliteMapStyle ? [244, 63, 94] : [100, 116, 139];
+      },
+    });
+  }, [data, isSatelliteMapStyle, selectedIndex]);
 
   const layers = [ScatterPlayLayer];
 
@@ -187,6 +193,72 @@ export default function DeckMap() {
       });
     }
   }, [selectedIndex, data]);
+
+  function getSearchSuggestions() {
+    function getSuggestionFooter() {
+      if (data.length > searchSuggestionCount) {
+        return (
+          <div className="p-4">
+            <Button
+              onClick={() => {
+                setSearchSuggestionCount((prev) => prev + 15);
+              }}
+              variant="outline"
+              className="w-full"
+            >
+              <Plus className="mr-2 h-4 w-4" />
+              Show more
+            </Button>
+          </div>
+        );
+      } else if (searchSuggestionCount > INITIAL_SEARCH_SUGGESTION_COUNT) {
+        return (
+          <p className="p-2 text-center text-sm italic text-neutral-500">
+            All items in view
+          </p>
+        );
+      }
+    }
+    if (
+      debouncedSearchValue &&
+      searchValue &&
+      suggestionsVisible &&
+      !selectedIndex
+    ) {
+      return (
+        <>
+          <CommandGroup heading={`Results (${data.length} items)`}>
+            {data.slice(0, searchSuggestionCount).map((entry) => (
+              <CommandItem
+                key={entry.id}
+                onSelect={() => {
+                  setSearchValue(entry.prettyLocation);
+                  setSelectedIndex(entry.id);
+                  console.log(entry);
+                }}
+              >
+                <div className="flex flex-col">
+                  <span>{entry.prettyLocation.toUpperCase()}</span>
+                  <div className="flex flex-wrap">
+                    {entry.grantee.split(",").map((grantee) => {
+                      return (
+                        <span key={grantee}>
+                          <Badge variant="outline">
+                            <span className="text-[10px]">{grantee}</span>
+                          </Badge>
+                        </span>
+                      );
+                    })}
+                  </div>
+                </div>
+              </CommandItem>
+            ))}
+          </CommandGroup>
+          {getSuggestionFooter()}
+        </>
+      );
+    }
+  }
 
   return (
     <>
@@ -274,36 +346,7 @@ export default function DeckMap() {
           />
           <CommandList>
             {/* {searchValue && <CommandEmpty>No results found.</CommandEmpty>} */}
-            {debouncedSearchValue &&
-              searchValue &&
-              suggestionsVisible &&
-              !selectedIndex &&
-              data.slice(0, 15).map((entry) => (
-                <CommandItem
-                  key={entry.id}
-                  onSelect={() => {
-                    setSearchValue(entry.prettyLocation);
-                    setSelectedIndex(entry.id);
-                    console.log(entry);
-                  }}
-                >
-                  <div className="flex flex-col">
-                    <span>{entry.prettyLocation.toUpperCase()}</span>
-                    {/* <span className="text-xs">{entry.grantee}</span> */}
-                    <div className="flex flex-wrap">
-                      {entry.grantee.split(",").map((grantee) => {
-                        return (
-                          <span key={grantee}>
-                            <Badge variant="outline">
-                              <span className="text-[10px]">{grantee}</span>
-                            </Badge>
-                          </span>
-                        );
-                      })}
-                    </div>
-                  </div>
-                </CommandItem>
-              ))}
+            {getSearchSuggestions()}
           </CommandList>
         </Command>
       </div>
