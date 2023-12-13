7755441fc38b1e2b9de1ee207eac520ce0f2d545
Dennis <dennis@momentum.local>
2023-12-13 14:47:26 -0800

    move mixpanel to a separate file and try making the botom sheet scrollable

--- src/components/BottomSheet.tsx
+++ src/components/BottomSheet.tsx
@@ -14,6 +14,9 @@ export type BottomSheetProps = {
 
 import type { PropsWithChildren } from "react";
 import { titleCase } from "title-case";
+import clsx from "clsx";
+
+const snapPoints = [0.45, 0.95];
 
 export default function BottomSheet(
   props: BottomSheetProps & PropsWithChildren,
@@ -21,32 +24,48 @@ export default function BottomSheet(
   const { open, onClose, location, grantee, grantor, children } = props;
   const [snap, setSnap] = useState<number | string | null>("148px");
 
+  const [activeSnapPoint, setActiveSnapPoint] = React.useState<
+    number | string | null
+  >(snapPoints[0]);
+
   return (
     <Drawer.Root
       open={open}
       onClose={onClose}
       modal={false}
       snapPoints={[0.45, 0.95]}
-      activeSnapPoint={snap}
-      setActiveSnapPoint={setSnap}
+      activeSnapPoint={activeSnapPoint}
+      setActiveSnapPoint={setActiveSnapPoint}
       closeThreshold={0.1}
       fadeFromIndex={1}
     >
       <Drawer.Portal>
         <Drawer.Content className="border-b-none fixed bottom-0 left-0 right-0 mx-[-1px] flex h-full max-h-[97%] flex-col rounded-t-[10px] border border-gray-200 bg-white">
-          <Drawer.NestedRoot>
-            <div className="flex-1 rounded-t-[10px] bg-white p-4">
+          <div
+            style={{ scrollbarGutter: "stable" }} // Stops scrollbar from moving the text
+            className="mx-auto flex max-h-full w-full w-screen flex-col p-4 pt-2"
+          >
+            <Drawer.NestedRoot>
               <div className="mx-auto mb-4 h-1.5 w-12 flex-shrink-0 overflow-auto rounded-full bg-zinc-300" />
-              <div className="mx-auto flex w-full max-w-md flex-col rounded-t-[10px]">
-                <Drawer.Title className="text-xl font-medium">
-                  {location && titleCase(location.toLowerCase())}
-                </Drawer.Title>
-                <br />
-                <Drawer.Description>{children}</Drawer.Description>
-                <Drawer.Close />
+              <div
+                className={clsx("text-gray-600", {
+                  "overflow-y-auto": activeSnapPoint === snapPoints[1],
+                  "overflow-hidden": activeSnapPoint !== snapPoints[1],
+                })}
+              >
+                <div className="flex-1 rounded-t-[10px] bg-white">
+                  <div className="mx-auto flex w-full max-w-md flex-col rounded-t-[10px]">
+                    <Drawer.Title className="text-xl font-medium">
+                      {location && titleCase(location.toLowerCase())}
+                    </Drawer.Title>
+                    <br />
+                    <Drawer.Description>{children}</Drawer.Description>
+                    <Drawer.Close />
+                  </div>
+                </div>
               </div>
-            </div>
-          </Drawer.NestedRoot>
+            </Drawer.NestedRoot>
+          </div>
         </Drawer.Content>
         <Drawer.Overlay />
       </Drawer.Portal>
